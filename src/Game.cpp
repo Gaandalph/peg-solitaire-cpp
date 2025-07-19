352391280f81a2b44ea4e9fcd9c65cda2c557548
Gaandalph <kaanarslan04@gmail.com>
2025-07-19 20:15:08 +0300

    Add basic game methods and implement some of them.

--- src/Game.cpp
+++ src/Game.cpp
@@ -3,3 +3,74 @@
 //
 
 #include "Game.h"
+
+Game:: Game() : rows(7), cols(7), pegsLeft(0), gameOver(false) {
+    initializeBoard();
+}
+
+Game::Game(int r, int c) : rows(r), cols(c), pegsLeft(0), gameOver(false) {
+    board.resize(rows, vector<int>(cols, -1));
+    initializeBoard();
+}
+
+Game:: ~Game() {
+
+}
+
+void Game::initializeBoard() {
+    board.assign(rows, vector<int>(cols, -1));
+
+    for (int i = 0 ; i < rows ; i++) {
+        for (int j = 0 ; j < cols ; j++) {
+            if ((i >= 2 && i <= 4) || (j >= 2 && j <= 4)) {
+                board[i][j] = 1 ;
+                pegsLeft++;
+            }
+        }
+    }
+
+    boards[3][3] = 0;
+    pegsLeft--;
+
+    gameOver = false;
+}
+
+void Game::resetGame() {
+    initializeBoard();
+}
+
+bool Game::isValidPosition(int row, int col) const {
+    return (row >= 0 && row < rows && col >= 0 && col < cols && board[row][col] != -1);
+}
+
+bool Game::isValidMove(int fromRow, int fromCol, int toRow, int toCol) const {
+    if (!isValidPosition(fromRow, fromCol) || board[fromRow][fromCol] == 1)
+        return false;
+
+    if (!isValidPosition(toRow, toCol) || board[toRow][toCol] != 0)
+        return false;
+
+    int midRow = (fromRow + toRow)/2;
+    int midCol = (fromCol + toCol)/2;
+
+    return board[midRow][midCol] == 1 ;
+}
+
+bool Game::makeMove(int fromRow, int fromCol, int toRow, int toCol) {
+    if (!isValidMove(fromRow, fromCol, toRow, toCol))
+        return false;
+
+    board[fromRow][fromCol] = 0 ;
+    board[toRow][toCol] = 1 ;
+
+    int midRow = (fromRow + toRow)/2;
+    int midCol = (fromCol + toCol)/2;
+    board[midRow][midCol] = 0;
+    pegsLeft--;
+
+    if (!hasValidMoves()) {
+        gameOver = true;
+    }
+
+    return true;
+}
--- src/Game.h
+++ src/Game.h
@@ -4,10 +4,56 @@
 
 #ifndef GAME_H
 #define GAME_H
-
+#include <vector>
+using namespace std;
 
 
 class Game {
+private:
+    vector<vector<char>> board;
+
+    int rows;
+    int cols;
+
+    int pegsLeft;
+
+    bool gameOver;
+
+    bool isValidPosition(int row, int col) const;
+    bool isValidMove(int fromRow, int fromCol, int toRow, int toCol) const;
+    void removePeg(int row, int col);
+    void addPeg(int row, int col);
+
+public:
+
+    Game();
+
+    Game(int rows, int cols);
+
+    ~Game();
+
+    void initializeBoard();
+    void resetGame();
+
+    bool makeMove(int fromRow, int fromCol, int toRow, int toCol);
+
+    bool isGameWon() const;
+    bool isGameOver() const;
+    bool hasValidMoves() const;
+
+
+    void displayBoard() const;
+    void printBoard() const;
+
+    int getPegsLeft() const;
+    int getBoardRows() const;
+    int getBoardCols() const;
+
+    vector<vector<int>> getBoard() const;
+
+    vector<pair<pair<int,int>, pair<int, int>>> getValidMoves() const;
+
+    void showGameStats() const;
 
 };
 
